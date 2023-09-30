7d8cbb93f4d16ab869a5de3caf228915c4caaf15
suchitrakhemnar <suchitrakhemnar@gmail.com>
2023-09-30 20:07:47 +0530

    did formatting to the file

--- src/main/java/com/sk/cutomermanagement/CutomerManagementApplication.java
+++ src/main/java/com/sk/cutomermanagement/CutomerManagementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CutomerManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CutomerManagementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CutomerManagementApplication.class, args);
+  }
 }
--- src/main/java/com/sk/cutomermanagement/entity/Customer.java
+++ src/main/java/com/sk/cutomermanagement/entity/Customer.java
@@ -5,69 +5,75 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "customer")
 public class Customer {
-    @Id
-    @Column(name = "cus_id", length = 20)
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int customerId;
+  @Id
+  @Column(name = "cus_id", length = 20)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int customerId;
 
-    @Column(name = "cus_name", length = 255)
-    private String customerName;
+  @Column(name = "cus_name", length = 255)
+  private String customerName;
 
-    @Column(name = "cus_email", length = 255)
-    private String email;
+  @Column(name = "cus_email", length = 255)
+  private String email;
 
-    @Column(name = "cus_password", length = 255)
-    private String password;
+  @Column(name = "cus_password", length = 255)
+  private String password;
 
-    public Customer() {
-    }
+  public Customer() {}
 
-    public Customer(int userId, String userName, String email, String password) {
-        this.customerId = userId;
-        this.customerName = userName;
-        this.email = email;
-        this.password = password;
-    }
+  public Customer(int userId, String userName, String email, String password) {
+    this.customerId = userId;
+    this.customerName = userName;
+    this.email = email;
+    this.password = password;
+  }
 
-    public int getUserId() {
-        return customerId;
-    }
+  public int getUserId() {
+    return customerId;
+  }
 
-    public void setUserId(int userId) {
-        this.customerId = userId;
-    }
+  public void setUserId(int userId) {
+    this.customerId = userId;
+  }
 
-    public String getUserName() {
-        return customerName;
-    }
+  public String getUserName() {
+    return customerName;
+  }
 
-    public void setUserName(String userName) {
-        this.customerName = userName;
-    }
+  public void setUserName(String userName) {
+    this.customerName = userName;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", customerName='" + customerName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Customer{"
+        + "customerId="
+        + customerId
+        + ", customerName='"
+        + customerName
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", password='"
+        + password
+        + '\''
+        + '}';
+  }
 }
