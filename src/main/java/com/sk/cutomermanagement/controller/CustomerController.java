4f00cebcbff555891ffe3517e2c0d4294fb4a1bd
suchitrakhemnar <suchitrakhemnar@gmail.com>
2023-09-30 10:49:17 +0530

    added and tested simple greet api

---
+++ src/main/java/com/sk/cutomermanagement/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.sk.cutomermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hello!";
+    }
+}
