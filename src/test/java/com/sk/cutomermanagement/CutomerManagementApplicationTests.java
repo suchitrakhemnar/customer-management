557dbd18d59798f829dbc33a29a2898aa138443e
suchitrakhemnar <suchitrakhemnar@gmail.com>
2023-09-30 10:40:47 +0530

    initial commit

---
+++ src/test/java/com/sk/cutomermanagement/CutomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.cutomermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
